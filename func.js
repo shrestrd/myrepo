1c76c3ce36e543a59fa459b00e66a76cf925b416
rims <rdshrest@gmail.com>
2023-04-05 14:48:22 -0400

    Fixed error

--- func.js
+++ func.js
@@ -1,8 +1,8 @@
 // Standard Normal variate using Box-Muller transform.
-function gaussianRandom(mean=0, stdev=1, se=1) {
+function gaussianRandom(mean=0, stdev=1) {
     let u = 1 - Math.random(); // Converting [0,1) to (0,1]
     let v = Math.random();
-    let z = Math.sqrt( -3.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    let z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
     // Transform to the desired mean and standard deviation:
     return z * stdev + mean;
 }
